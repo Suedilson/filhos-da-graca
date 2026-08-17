ae8e5dbced506e14864aa94533eea6349093c77c
Suedilson Filho <suedilsonfilho@gmail.com>
2026-08-16 23:07:42 -0300

    Corrige template de producao do deploy

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -55,7 +55,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'index.html'),
+      template: path.resolve(__dirname, 'index-webpack.html'),
       minify: {
         collapseWhitespace: true,
         removeComments: true,
